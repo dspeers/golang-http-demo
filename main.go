22f842c9827600a652a6257d01fdac563e1eb82c
Dan Speers <dan@speers.nu>
2018-05-09 13:38:22 -0400

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example - Speers LABS"
+	title := "Jenkins X golang http example - Speers Home LAB"
 
 	from := ""
 	if r.URL != nil {
